8d342e61b7506639740f20ffb04a9d8a8554d9b1
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-06-30 19:50:54 +0300

    edit router.js

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,6 +2,10 @@ import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     {
+        path: '/',
+        redirect: '/form/first_step'
+    },
+    {
         path: '/form/first_step', component: () => import('./components/Form/FirstStep.vue'),
         name: 'first.step'
     },
@@ -10,8 +14,16 @@ const routes = [
         name: 'second.step'
     },
     {
+        path: '/form/third_step', component: () => import('./components/Form/ThirdStep.vue'),
+        name: 'third.step'
+    },
+    {
         path: '/all_members', component: () => import('./components/AllMembers.vue'),
         name: 'all.members'
+    },
+    {
+        path: '/admin/login', component: () => import('./components/AdminLogin.vue'),
+        name: 'admin.login'
     },
 ]
 
