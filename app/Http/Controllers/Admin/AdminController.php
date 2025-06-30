1498409d0b575ce818a8f4a933cd2b405e276e23
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-06-30 19:47:58 +0300

    Add methods: update, toggleVisibility, delete

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,16 +3,45 @@
 namespace App\Http\Controllers\Admin;
 
 
-use App\Http\Requests\Form\FirstStepRequest;
-use App\Http\Requests\Form\SecondStepRequest;
+use App\Http\Requests\Admin\MemberUpdateRequest;
 use App\Models\Member;
+use App\Services\FileService;
 
 class AdminController
 {
+    public function update(MemberUpdateRequest $request, Member $member)
+    {
+        $data = $request->validated();
+
+        $nameParts = preg_split('/\s+/', trim($data['full_name']));
+
+        $data['first_name'] = $nameParts[0];
+        $data['last_name'] = $nameParts[1];
+        unset($data['full_name']);
+
+        $photoPath = FileService::photoService($request, $member);
+        $data['photo'] = $photoPath;
+
+        $member->update($data);
+
+        return response()->json(['success' => true]);
+    }
+
+    public function toggleVisibility(Member $member)
+    {
+        $member->visibility = !$member->visibility;
+        $member->save();
+
+        return response()->json([
+            'success' => true,
+            'visible' => $member->visibility,
+        ]);
+    }
+
     public function delete(Member $member)
     {
         $member->delete();
 
-        return response([]);
+        return response()->json(['success' => true]);
     }
 }
