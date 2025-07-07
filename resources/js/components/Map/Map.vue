9909a3a50cacd06afd1fadd8b63dee39739fa7e7
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-07 18:28:08 +0300

    change mounted() to onMounted()

--- resources/js/components/Map/Map.vue
+++ resources/js/components/Map/Map.vue
@@ -1,9 +1,14 @@
-<script>
+<template>
+    <div id="map" class="w-full h-[450px]"></div>
+</template>
+
+<script setup>
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import {onMounted} from "vue";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -13,20 +18,13 @@ L.Icon.Default.mergeOptions({
     shadowUrl: markerShadow,
 });
 
-export default {
-    name: 'MapComponent',
-    mounted() {
-        const map = L.map('map').setView([34.101558, -118.342345], 15);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; OpenStreetMap contributors'
-        }).addTo(map);
-        L.marker([34.101558, -118.342345]).addTo(map)
-            .bindPopup('7060 Hollywood Blvd, Los Angeles, CA')
-            .openPopup();
-    },
-};
+onMounted(() => {
+    const map = L.map('map').setView([34.101558, -118.342345], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+    L.marker([34.101558, -118.342345]).addTo(map)
+        .bindPopup('7060 Hollywood Blvd, Los Angeles, CA')
+        .openPopup();
+});
 </script>
-
-<template>
-    <div id="map" class="w-full h-[450px]"></div>
-</template>
