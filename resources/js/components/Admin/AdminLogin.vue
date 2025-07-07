7f6a172b3f39cf8341019aa44ea2337881db8c3b
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-07 18:24:57 +0300

    change inputs to BaseInput components

--- resources/js/components/Admin/AdminLogin.vue
+++ resources/js/components/Admin/AdminLogin.vue
@@ -2,39 +2,32 @@
     <div class="max-w-md mx-auto mt-20 mb-20 p-6 bg-white shadow rounded">
         <h2 class="text-xl font-semibold mb-4 text-center">Login</h2>
 
-        <div class="flex flex-col space-y-4">
+        <form @submit.prevent="login" class="flex flex-col space-y-4">
             <div class="flex flex-col">
-                <input
-                    type="text"
+                <BaseInput
                     placeholder="email"
                     v-model="formData.email"
-                    class="border p-2 rounded w-full outline-none focus-within:border-gray-800 focus:border-gray-800"
+                    :errors="errors"
                 />
-                <div v-if="errors.email" class="text-red-600 text-sm mt-1">
-                    {{ errors.email }}
-                </div>
             </div>
 
             <div class="flex flex-col">
-                <input
+                <BaseInput
                     type="password"
                     placeholder="password"
                     v-model="formData.password"
-                    class="border p-2 rounded w-full outline-none focus-within:border-gray-800 focus:border-gray-800"
+                    :errors="errors"
                 />
-                <div v-if="errors.password" class="text-red-600 text-sm mt-1">
-                    {{ errors.password }}
-                </div>
             </div>
-        </div>
 
-        <div class="text-right pt-6">
-            <button
-                @click.prevent="login"
-                class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded">
-                Login
-            </button>
-        </div>
+            <div class="text-right pt-6">
+                <button
+                    type="submit"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded">
+                    Login
+                </button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -43,6 +36,7 @@
 
 import {inject, ref} from "vue";
 import router from "../../router.js";
+import BaseInput from "../BaseInput.vue";
 
 const showErrors = inject('showErrors')
 const isAdmin = inject('isAdmin')
