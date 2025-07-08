47dc67a5ea27f9e85c85b425e33f111f7adc467f
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-08 19:02:43 +0300

    add BaseTable.vue

---
+++ resources/js/components/UI/Form/BaseTable.vue
@@ -0,0 +1,24 @@
+<template>
+    <table class="min-w-full border border-gray-300 rounded overflow-hidden table-auto">
+        <thead class="bg-gray-100">
+        <tr>
+            <th
+                v-for="(header, index) in headers"
+                :key="index"
+                class="p-3 text-left border-b text-sm text-gray-700"
+            >
+                {{ header }}
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <slot/>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+defineProps({
+    headers: Array
+});
+</script>
