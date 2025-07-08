4491a39204b898d2079f16828cff7d0482dacab9
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-08 19:03:48 +0300

    add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 import './bootstrap.js';
 import { createApp } from 'vue';
+import { createPinia } from 'pinia'
 import router from './router.js'
 import App from './components/App.vue'
 
-const app = createApp(App);
-app.use(router);
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+
 app.mount('#app');
