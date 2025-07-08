2f391be7f781a3a50cf2f83bc59fab43caa75435
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-08 19:07:33 +0300

    add MemberRow.vue, MemberRowEdit.vue

--- resources/js/components/MemberRow.vue
+++ resources/js/components/MemberRow.vue
@@ -1,16 +1,16 @@
 <template>
-    <tr class="border-b">
+    <tr class="border-b text-sm">
         <th class="hidden">{{ member.id }}</th>
         <td class="p-3">
             <img
                 :src="`/storage/${member.photo}`"
                 :alt="member.full_name"
-                class="h-12 w-12 object-cover rounded-full"
+                class="h-10 w-10 object-cover rounded-full"
             />
         </td>
-        <td class="p-3 break-words">{{ member.full_name }}</td>
-        <td class="p-3 break-words">{{ member.report_subject }}</td>
-        <td class="p-3 break-words">
+        <td class="p-3 break-words max-w-[200px]">{{ member.full_name }}</td>
+        <td class="p-3 break-words max-w-[200px]">{{ member.report_subject }}</td>
+        <td class="p-3 break-all max-w-[200px]">
             <a :href="`https://mail.google.com/mail/?view=cm&fs=1&to=${member.email}`"
                target="_blank"
                class="text-blue-600 hover:underline"
@@ -18,7 +18,7 @@
                 {{ member.email }}
             </a>
         </td>
-        <td v-if="isAdmin" class="p-3">
+        <td v-if="isAdmin" class="p-3 whitespace-nowrap">
             <button
                 @click.prevent="$emit('edit', member)"
                 class="underline text-orange-600"
@@ -26,7 +26,7 @@
                 Edit
             </button>
         </td>
-        <td v-if="isAdmin" class="p-3">
+        <td v-if="isAdmin" class="p-3 whitespace-nowrap">
             <button
                 @click="$emit('delete', member.id)"
                 class="underline text-red-600"
@@ -34,7 +34,7 @@
                 Delete
             </button>
         </td>
-        <td v-if="isAdmin" class="p-3">
+        <td v-if="isAdmin" class="p-3 whitespace-nowrap">
             <button
                 @click="$emit('toggle', member)"
                 :class="member.visibility ? 'text-green-600' : 'text-gray-400'"
--- resources/js/components/MemberRowEdit.vue
+++ resources/js/components/MemberRowEdit.vue
@@ -1,12 +1,12 @@
 <template>
-    <tr class="border-b bg-white hover:bg-gray-50">
+    <tr class="border-b bg-white hover:bg-gray-50 text-sm">
         <th class="hidden">{{ member.id }}</th>
         <td class="p-3">
-            <label class="block w-12 h-12 relative">
+            <label class="block w-10 h-10 relative">
                 <img
                     :src="photoPreview || `/storage/${member.photo}`"
                     alt="photo"
-                    class="w-12 h-12 object-cover rounded-full"
+                    class="h-10 w-10 object-cover rounded-full"
                 />
                 <FileInput
                     name="photo"
@@ -19,23 +19,23 @@
             </label>
 
         </td>
-        <td class="p-3">
+        <td class="p-3 break-words max-w-[200px]">
             <BaseInput
-                name="fullName"
+                name="full_name"
                 :modelValue="fullName"
                 @update:modelValue="emit('update:fullName', $event)"
                 :errors="errors"
             />
         </td>
-        <td class="p-3">
+        <td class="p-3 break-words max-w-[200px]">
             <BaseInput
-                name="reportSubject"
+                name="report_subject"
                 :modelValue="reportSubject"
                 @update:modelValue="emit('update:reportSubject', $event)"
                 :errors="errors"
             />
         </td>
-        <td class="p-3">
+        <td class="p-3 break-words max-w-[200px]">
             <BaseInput
                 name="email"
                 :modelValue="email"
@@ -43,14 +43,14 @@
                 :errors="errors"
             />
         </td>
-        <td class="p-3">
+        <td class="p-3 whitespace-nowrap">
             <button
                 class="inline-block bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700 text-sm"
                 @click="$emit('update', member.id)">
                 Update
             </button>
         </td>
-        <td class="p-3">
+        <td class="p-3 whitespace-nowrap">
             <button
                 class="ml-2 text-sm text-gray-500 hover:underline"
                 @click="$emit('cancelEdit')">
