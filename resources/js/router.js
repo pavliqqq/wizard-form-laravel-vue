119c1f327f4880ff600933b75d494cd975cd9a4e
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-10 18:57:22 +0300

    change routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,35 +1,30 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
-        path: '/',
-        redirect: '/form/first_step'
+        path: "/",
+        redirect: "/form",
     },
     {
-        path: '/form/first_step', component: () => import('./components/Form/FirstStep.vue'),
-        name: 'first.step'
+        path: "/form",
+        component: () => import("./components/Form/MemberForm.vue"),
+        name: "member.form",
     },
     {
-        path: '/form/second_step', component: () => import('./components/Form/SecondStep.vue'),
-        name: 'second.step'
+        path: "/all_members",
+        component: () => import("./components/AllMembers/AllMembers.vue"),
+        name: "all.members",
     },
     {
-        path: '/form/third_step', component: () => import('./components/Form/ThirdStep.vue'),
-        name: 'third.step'
+        path: "/admin/login",
+        component: () => import("./components/Admin/AdminLogin.vue"),
+        name: "admin.login",
     },
-    {
-        path: '/all_members', component: () => import('./components/AllMembers/AllMembers.vue'),
-        name: 'all.members'
-    },
-    {
-        path: '/admin/login', component: () => import('./components/Admin/AdminLogin.vue'),
-        name: 'admin.login'
-    },
-]
+];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
-})
+});
 
 export default router;
