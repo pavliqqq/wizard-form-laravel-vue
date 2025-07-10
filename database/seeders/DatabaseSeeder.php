4af4bacb51f9b8e413b9ad5e3c89ca926c18f725
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-10 18:58:12 +0300

    add model Country, laravel pint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CountrySeeder::class,
-            AdminSeeder::class
+            AdminSeeder::class,
         ]);
     }
 }
