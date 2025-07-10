7d0499e1ec80ec1f84626d44174671da9133b489
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-10 18:54:56 +0300

    change method for obj params

--- resources/js/helpers/request.js
+++ resources/js/helpers/request.js
@@ -1,11 +1,16 @@
-import {toSnakeCase} from "../utils/caseConverter.js";
+import { camelToSnake } from "./caseConverter.js";
 
 export function createFormData(obj) {
-    const convertObj = toSnakeCase(obj);
     const formData = new FormData();
 
-    for (const [key, value] of Object.entries(convertObj)) {
-        formData.append(key, value);
+    for (const [key, value] of Object.entries(obj)) {
+        const snakeKey = camelToSnake(key);
+
+        if (value === null) {
+            formData.append(snakeKey, "");
+        } else {
+            formData.append(snakeKey, value);
+        }
     }
 
     return formData;
