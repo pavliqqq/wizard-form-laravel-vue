5a77b448993fe150fdd2b7d66b69eb9f528af4a9
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-10 19:03:28 +0300

    edit App.vue: move from FormComponent to App.vue, import AdminStore

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,10 +1,31 @@
 <template>
     <div id="app">
-        <FormComponent></FormComponent>
+        <Map v-if="showMap"></Map>
+        <div class="mt-3 mr-3 text-right">
+            <LogoutButton v-if="adminStore.isAdmin" />
+        </div>
+        <router-view />
     </div>
 </template>
 
-
 <script setup>
-import FormComponent from './FormComponent.vue';
+import { computed, onMounted } from "vue";
+import LogoutButton from "./Admin/LogoutButton.vue";
+import Map from "./Map/Map.vue";
+import { useRoute } from "vue-router";
+import { useAdminStore } from "../stores/adminStore.js";
+
+const adminStore = useAdminStore();
+
+onMounted(() => {
+    adminStore.isAdmin = !!localStorage.getItem("token");
+});
+
+const route = useRoute();
+
+const mapRoute = "member.form";
+
+const showMap = computed(() => {
+    return mapRoute === route.name;
+});
 </script>
