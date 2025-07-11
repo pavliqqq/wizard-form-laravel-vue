19e86ff708c88e69f826be10265682cdf577b0b4
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-11 19:02:57 +0300

    rename AdminSeeder to UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CountrySeeder::class,
-            AdminSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
