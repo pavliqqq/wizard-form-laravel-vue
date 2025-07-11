b0fa30fe70f3768c221b27ee7ccbc8d61c204a11
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-11 19:02:47 +0300

    rename AdminSeeder to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,19 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class AdminSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Admin::create([
+        User::create([
+            'name' => 'admin',
             'email' => 'admin@gmail.com',
+            'role' => 'admin',
             'password' => Hash::make('12345'),
         ]);
     }
