791d76d985a6f1a5e2a57cdb380777acee636a7f
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-14 12:35:12 +0300

    add console command for default-image

---
+++ app/Console/Commands/CopyDefaultImage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\FileService;
+use Illuminate\Console\Command;
+
+class CopyDefaultImage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:default-image';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Copy default image to storage';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        FileService::defaultImageCopy();
+        $this->info('Default image copied successfully or already exists.');
+        return self::SUCCESS;
+    }
+}
