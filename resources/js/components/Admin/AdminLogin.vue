4b22944f3285b8384de6cb62f6b135d35e9023fa
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-14 12:43:28 +0300

    add text for error 403

--- resources/js/components/Admin/AdminLogin.vue
+++ resources/js/components/Admin/AdminLogin.vue
@@ -1,36 +1,38 @@
 <template>
-    <div class="max-w-md mx-auto mt-20 mb-20 p-6 bg-white shadow rounded">
-        <h2 class="text-xl font-semibold mb-4 text-center">Login</h2>
+    <div class="max-w-7xl mx-auto mt-10 mb-10 px-4 sm:px-6 lg:px-8">
+        <div class="bg-white shadow rounded p-4 sm:p-6 max-w-md mx-auto">
+            <h2 class="text-lg sm:text-xl text-center font-semibold mb-4">Login</h2>
 
-        <form @submit.prevent="login" class="flex flex-col space-y-4">
-            <div class="flex flex-col">
-                <BaseInput
-                    name="email"
-                    placeholder="email"
-                    v-model="Data.email"
-                    :errors="errors"
-                />
-            </div>
+            <form @submit.prevent="login" class="flex flex-col space-y-4">
+                <div class="flex flex-col">
+                    <BaseInput
+                        name="email"
+                        placeholder="email"
+                        v-model="Data.email"
+                        :errors="errors"
+                    />
+                </div>
 
-            <div class="flex flex-col">
-                <BaseInput
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                    v-model="Data.password"
-                    :errors="errors"
-                />
-            </div>
+                <div class="flex flex-col">
+                    <BaseInput
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        v-model="Data.password"
+                        :errors="errors"
+                    />
+                </div>
 
-            <div class="text-right pt-6">
-                <button
-                    type="submit"
-                    class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded"
-                >
-                    Login
-                </button>
-            </div>
-        </form>
+                <div class="text-right pt-6">
+                    <button
+                        type="submit"
+                        class="w-full sm:w-auto bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded text-sm sm:text-base"
+                    >
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -61,8 +63,13 @@ async function login() {
 
         await router.push({name: "all.members"});
     } catch (error) {
-        if (error.response && error.response.status === 422) {
-            errorStore.showErrors(error.response.data.errors);
+        if (error.response) {
+            if (error.response.status === 422) {
+                errorStore.showErrors(error.response.data.errors);
+            }
+            if (error.response.status === 403) {
+                errorStore.showErrors({email: [error.response.data.message]});
+            }
         }
     }
 }
