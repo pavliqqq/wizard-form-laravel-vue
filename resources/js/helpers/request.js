9eda59d98757b98df34fc078556134bc3d346f85
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-17 16:27:44 +0300

    edit func createFormData, add func appendFormData

--- resources/js/helpers/request.js
+++ resources/js/helpers/request.js
@@ -5,13 +5,28 @@ export function createFormData(obj) {
 
     for (const [key, value] of Object.entries(obj)) {
         const snakeKey = camelToSnake(key);
-
-        if (value === null) {
-            formData.append(snakeKey, "");
-        } else {
-            formData.append(snakeKey, value);
-        }
+        appendFormData(formData, value, snakeKey);
     }
 
     return formData;
+}
+
+function appendFormData(formData, value, key) {
+    if (value instanceof File) {
+        formData.append(key, value);
+    } else if (value instanceof Array) {
+        value.forEach((item, index) => {
+            const newKey = `${key}[${index}]`;
+            appendFormData(formData, item, newKey);
+        });
+    } else if (typeof value === "object") {
+        for (const [objKey, item] of Object.entries(value)) {
+            const snakeKey = camelToSnake(objKey);
+
+            const newKey = `${key}[${snakeKey}]`;
+            appendFormData(formData, item, newKey);
+        }
+    } else {
+        formData.append(key, value === null ? "" : value);
+    }
 }
