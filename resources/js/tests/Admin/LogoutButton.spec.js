6e11fd218f99d4a16c6606739772b6f35601d63f
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-08-13 16:23:11 +0300

    update test 'logs out user and refresh page': add mockResolvedValue for axios.get and axios.post, add flushPromises create test 'does not reset value and reload page on logout error'

--- resources/js/tests/Admin/LogoutButton.spec.js
+++ resources/js/tests/Admin/LogoutButton.spec.js
@@ -1,4 +1,4 @@
-import { mount } from "@vue/test-utils";
+import {flushPromises, mount} from "@vue/test-utils";
 import router from "../../router.js";
 import LogoutButton from "../../components/Admin/LogoutButton.vue";
 import { createPinia, setActivePinia } from "pinia";
@@ -21,7 +21,7 @@ jest.mock("../../stores/adminStore.js", () => ({
     }),
 }));
 
-describe("LogoutButton.vue", () => {
+describe("Logout button", () => {
     let wrapper, logoutButton;
     beforeEach(() => {
         setActivePinia(createPinia());
@@ -36,12 +36,30 @@ describe("LogoutButton.vue", () => {
         expect(logoutButton.exists()).toBe(true);
     });
 
-    it("logs out user", async () => {
+    it("logs out user and refresh page", async () => {
+        axios.get.mockResolvedValue({});
+        axios.post.mockResolvedValue({ success: true });
+
         await logoutButton.trigger("click");
+
+        await flushPromises();
 
         expect(axios.get).toHaveBeenCalledWith("/sanctum/csrf-cookie");
         expect(axios.post).toHaveBeenCalledWith("/api/logout", null);
         expect(mockReset).toHaveBeenCalled();
+
         expect(router.go).toHaveBeenCalledWith(0);
     });
+
+    it("does not reset value and reload page on logout error", async() => {
+        jest.spyOn(console, "error").mockImplementation(() => {});
+        axios.post.mockRejectedValue(new Error("Network error"));
+
+        await logoutButton.trigger("click");
+
+        await flushPromises();
+
+        expect(mockReset).not.toHaveBeenCalled();
+        expect(router.go).not.toHaveBeenCalled();
+    })
 });
